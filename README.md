939b1ba0b0802ab3707a46b99563e216cc3032b0
wnw231423 <2684722187@qq.com>
2024-10-29 19:33:36 +0800

    lab05 finished.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# fa20-lab
+# CS61C-fa20 labs and projects 
+My changes are listed here:
+1. Replace the `venus.jar` file with the version provided by fa24.
+2. Replace the `logisim-evolution.jar` file with the latest release version.
--- lab05/test.sh
+++ lab05/test.sh
@@ -4,6 +4,7 @@
 # Make sure your files are in the directory above this one though!
 
 cp ex1.circ ex2.circ ex3.circ ex4.circ ex5.circ testing/circ_files
+cp ../tools/logisim-evolution-3.9.0-all.jar testing/logisim-evolution.jar
 
 cd testing
 rm -rf student_output
